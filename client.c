ca650e94a661e1dda4dbd11621389ee4d9fc52fc
takayasumorishita <c011729632@edu.teu.ac.jp>
2021-07-05 23:19:36 +0900

    Update client

--- client.c
+++ client.c
@@ -5,9 +5,9 @@
 #include <string.h>
 #include <unistd.h>
 
-#define MSGSIZE 32
-#define MAX_MSGSIZE 1024
-#define BUFSIZE (MSGSIZE + 1)
+#define MESSAGE_SIZE 32
+#define MAX_MESSAGE_SIZE 1024
+#define BUFSIZE (MESSAGE_SIZE + 1)
 
 void die(const char *msg) {
     perror(msg);
@@ -18,47 +18,48 @@ int main(int argc, const char *argv[])
 {
     int err;
 
-    int socket;
+    int sock;
     struct sockaddr_in serverSocketAddress;
     unsigned short serverPortNumber;
-    char recvBuffer[BUFSIZE];
-    int receivedByteSize, totalReceivedByteSize;
+    char recivedBuffer[BUFSIZE];
+    int receivedBufferByteSize, totalReceivedBufferByteSize;
 
     if (argc != 3) die("Argument count is mismatch");
-
-    serverPortNumber = (unsigned short)atoi(argv[1]);
-    if (!serverPortNumber) die("Invalid port number");
 
     memset(&serverSocketAddress, 0, sizeof(serverSocketAddress));
 
     serverSocketAddress.sin_family = AF_INET;
 
-    serverSocketAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    err = inet_aton(argv[1], &serverSocketAddress.sin_addr);
+    if (err == 0) die("Invalid IP Address.\n");
+
+    serverPortNumber = (unsigned short)atoi(argv[2]);
+    if (serverPortNumber == 0) die("Invalid Port Number.\n");
     serverSocketAddress.sin_port = htons(serverPortNumber);
-    
-    err = bind(
-        serverSocket,
-        (struct sockaddr *)&serverSocketAddress,
-        sizeof(serverSocketAddress)
-        );
-    if (err < 0) die("bind() failed");
 
-    err = listen(serverSocket, LIMIT_OF_QUEUE);
-    if (err < 0) die("listen() failed");
+    sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (err < 0) die("socket() failed.");
 
-    while (1)
+    err = connect(sock, (struct sockaddr*) &serverSocketAddress, sizeof(serverSocketAddress));
+    if (err < 0) die("connect() failed.");
+
+    printf("Connect tt %s\n", inet_ntoa(serverSocketAddress.sin_addr));
+
+    totalReceivedBufferByteSize = 0;
+    while (totalReceivedBufferByteSize < MAX_MESSAGE_SIZE)
     {
-        clientAddressLength = sizeof(clientSocketAddress);
-        clientSocket = accept(
-            serverSocket,
-            (struct sockaddr *)&clientSocketAddress,
-            &clientAddressLength
-            ); 
-        if (clientSocket < 0) die("accept() failed");
-
-        printf("Connected from %s\n", inet_ntoa(clientSocketAddress.sin_addr));
-        close(clientSocket);
+        receivedBufferByteSize = recv(sock, recivedBuffer, MESSAGE_SIZE, 0);
+        if (receivedBufferByteSize > 0) {
+            recivedBuffer[receivedBufferByteSize] = '\0';
+            printf("%s", recivedBuffer);
+            totalReceivedBufferByteSize += receivedBufferByteSize;
+        } 
+        else if (receivedBufferByteSize == 0) die("ERR_EMPTY_RESPONSE");
+        else die("recv() failed.");
     }
+    printf("\n");
+
+    close(sock);
 
     return EXIT_SUCCESS;
 }
