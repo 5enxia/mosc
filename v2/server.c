395a5f545086afd3ea053f02ae050b6a2ff8307c
5enxia <takayasumorishita@gmail.com>
2023-03-08 23:54:14 +0900

    Add v2/server.c

---
+++ v2/server.c
@@ -0,0 +1,23 @@
+#include <sys/param.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include <ctype.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
